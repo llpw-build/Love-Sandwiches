e067032dded943cb9dd8afbe19a04c38a379e983
llpw-build <llpw@hotmail.co.uk>
2026-03-30 16:29:44 +0100

    Add function to change string to integers and update worksheet

--- run.py
+++ run.py
@@ -14,7 +14,8 @@ SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
 def get_sales_data():
     """"
-    Get sales figures input from the user
+    Get sales figures input from the user and also include a while
+    loop until accurate data is provided from the user.
     """
     while True:
         print("Please enter sales data from the last market.")
@@ -25,7 +26,7 @@ def get_sales_data():
    
         sales_data = data_str.split(",")
         print(sales_data)
-        validate_data(sales_data)
+        
 
         if validate_data(sales_data):
             print("Data is valid!")
@@ -52,6 +53,15 @@ def validate_data(values):
 
     return True
 
-
+def update_sales_worksheet(data):
+    """
+    Update the sales worksheet and add new row with the new data
+    """
+    print("Updating sales worksheet...\n")
+    sales_worksheet = SHEET.worksheet("sales")
+    sales_worksheet.append_row(data)
+    print("Sales worksheet updated successfully.\n")
 
 data = get_sales_data()
+sales_data = [int(num) for num in data]
+update_sales_worksheet(sales_data)
